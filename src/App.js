66087aac7781a248186429c2e6afd2c4bb6cf262
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-01-22 17:59:55 +0530

    Restructor useState code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import UseState1 from "./UseState1";
-import UseState2 from "./UseState2";
-import UseState3 from "./UseState3";
+import UseState1 from "./Component/UseStateVersion/UseState1";
+import UseState2 from "./Component/UseStateVersion/UseState2";
+import UseState3 from "./Component/UseStateVersion/UseState3";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Hooks</h1>
-      <UseState1 />
+      <UseState1></UseState1>
       <hr />
       <UseState2 />
       <hr />
